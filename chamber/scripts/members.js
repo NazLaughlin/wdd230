8be3d54defc11e46d3decdbab3a12c948344a4b4
Nazanal Laughlin <Nazanal@live.com>
2023-12-07 20:28:03 -0700

    Directory complete

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,4 +1,4 @@
-const list = document.querySelector("#links")
+const list = document.querySelector("#display")
 const baseURL = `https://nazlaughlin.github.io/wdd230`;
 const linksURL = `${baseURL}/chamber/data/members.json`;
 
@@ -26,12 +26,44 @@ async function displayMembers(members){
         
         const title = document.createElement("h3")
         title.textContent = member.name
-        title.classList.add("c1");
+
+        const level = document.createElement("p")
+        level.textContent = member.membership
 
         const address = document.createElement("p")
         address.textContent = member.address;
-        address.classList.add("c2")
+
+        const phone = document.createElement("p")
+        phone.textContent = member.phone
+
+        const website = document.createElement("a")
+        website.href = member.url
+        website.textContent = "Website"
+
+        memberSection.append(pic);
+        memberSection.append(title);
+        memberSection.append(level)
+        memberSection.append(address);
+        memberSection.append(phone)
+        memberSection.append(website)
+
+        list.appendChild(memberSection);
     });
 }
+
+function displayList(){
+    list.classList.add("list")
+    list.classList.remove("grid")
+}
+
+function displayGrid(){
+    list.classList.add("grid")
+    list.classList.remove("list")
+}
+
+const gridRadio = document.querySelector("#grid");
+const listRadio = document.querySelector("#list");
+gridRadio.addEventListener("click", displayGrid);
+listRadio.addEventListener("click", displayList)
 
 getMembers()
